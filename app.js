f2e6702c078d6f781efd1c884792fb2cdd9bce39
yongjin park <qkrdydwls3@gmail.com>
2022-07-03 21:47:35 +0900

    add passport

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+const mongoConnect = require('./util/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRoutuer = require('./routes/login');
-const mongoConnect = require('./util/database');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.set('port',process.env.PORT || 3000)
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+//social login
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const loginController = require('../controllers/login');
 const router = express.Router();
+const passport = require('passport');
+
+
+router.get('/naver', passport.authenticate('naver', { authType: 'reprompt' }));
+
+router.get(
+    '/naver/callback',
+    passport.authenticate('naver', { failureRedirect: '/' }),
+    (req, res) => {
+       res.redirect('/');
+    },
+ );
 
 router.get('/',loginController.login);
 
